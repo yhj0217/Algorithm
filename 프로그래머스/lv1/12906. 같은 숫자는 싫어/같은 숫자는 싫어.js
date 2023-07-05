5848f510020a50142165bbbff7bc5c2af70a0570
HeeJoon.Yang <112550610+yhj0217@users.noreply.github.com>
2023-07-05 17:35:39 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 11.66 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr){
+    var answer=[]
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i-1] !== arr[i]){
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
