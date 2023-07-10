be0c3effdb2b9619e7882a4c6a85ae4ecd7f1238
YangHeeJoon <112550610+yhj0217@users.noreply.github.com>
2023-07-10 18:02:22 +0900

    [level 1] Title: 예산, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    var answer = 0;
+    d.sort((a,b)=> a-b);
+    for(let i=0; i<d.length; i++){
+        if(budget>=d[i]){
+            budget = budget - d[i];
+            answer++;
+        }
+    }
+    return answer;
+}
