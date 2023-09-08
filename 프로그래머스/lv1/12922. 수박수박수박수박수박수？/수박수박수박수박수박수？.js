deabe88863563d795d56d6ee5d4010667a7a5b07
YangHeeJoon <112550610+yhj0217@users.noreply.github.com>
2023-09-08 23:03:44 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = '';
+    for(let i=1; i<=n; i++){
+        if(i%2 != 0){
+            answer = answer + '수'
+        }
+        else
+            answer = answer + '박'
+    }
+    return answer;
+}
