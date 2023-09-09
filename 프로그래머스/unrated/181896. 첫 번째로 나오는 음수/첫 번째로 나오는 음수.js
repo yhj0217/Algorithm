870872019e9862eb52a868c9d2cc2cdd2c43f0dd
YangHeeJoon <112550610+yhj0217@users.noreply.github.com>
2023-09-09 23:42:23 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,8 @@
+function solution(num_list) {
+    let answer = 0;
+    for(let i=0; i<num_list.length; i++){
+        if(num_list[i]<0)
+            return num_list.indexOf(num_list[i])
+    }
+    return -1;
+}
