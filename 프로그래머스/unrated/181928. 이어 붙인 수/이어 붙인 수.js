76906b9e6996244616ed6d052f3d9f8c9c9d86fc
YangHeeJoon <112550610+yhj0217@users.noreply.github.com>
2023-09-10 23:58:33 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let even = '';
+    let odd = '';
+    num_list.join('');
+    for(let i=0; i<num_list.length; i++) {
+        if(num_list[i] % 2 === 0) even += num_list[i]
+        else odd += num_list[i]
+    }
+    return parseInt(even) + parseInt(odd);
+}
