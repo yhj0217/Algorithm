7a066c8d3a9abeced7032d7de55c99f186a509c0
YangHeeJoon <112550610+yhj0217@users.noreply.github.com>
2023-11-03 23:44:50 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let answer = 0;
+    for(let i=1; i<=n; i++){
+        answer += 1;
+        while(answer.toString().includes('3') ||  answer % 3 == 0){            
+            answer += 1;
+        }
+    }
+    return answer;
+}
