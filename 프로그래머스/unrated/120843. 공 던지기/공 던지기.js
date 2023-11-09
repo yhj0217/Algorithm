16d40c90d9de132819a60c5c5d57eb903c9a16f9
YangHeeJoon <112550610+yhj0217@users.noreply.github.com>
2023-11-09 23:08:25 +0900

    [level unrated] Title: 공 던지기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.js
@@ -0,0 +1,13 @@
+function solution(numbers, k) {
+    var answer = 1;
+    for(let i=0; i<k-1; i++){
+        answer+=2;
+        if(answer>numbers.length){
+            answer-=numbers.length;
+        }
+            
+        }
+        
+    
+    return answer;
+}
