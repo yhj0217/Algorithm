18e05fe6b80ddd956d7efeae3a1410461127a655
YangHeeJoon <112550610+yhj0217@users.noreply.github.com>
2024-01-04 16:30:03 +0900

    [Silver IV] Title: 괄호, Time: 140 ms, Memory: 111372 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,23 @@
+test = int(input())
+
+for _ in range(test):
+    inputs = list(input())
+    stack = []
+    bool = True
+
+    for i in range(len(inputs)):
+        if inputs[i] == '(':
+            stack.append(inputs[i])
+        else:
+            if len(stack) == 0:
+                bool = False
+                break
+            else:
+                stack.pop()
+    if bool == True:
+        if len(stack) != 0:
+            print("NO")
+        else:
+            print("YES")
+    else:
+        print("NO")
