287294f3aec04b9b1edaa59272721311f905dedd
YangHeeJoon <112550610+yhj0217@users.noreply.github.com>
2024-01-15 18:44:37 +0900

    [level Lv.2] Title: 프로세스, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42587. 프로세스/프로세스.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    deq = deque(priorities)
+    
+    while deq:
+        first = max(deq)
+        temp = deq.popleft()
+        if first > temp:
+            deq.append(temp)
+            location -= 1
+            if location < 0:
+                location = len(deq) - 1
+        elif first == temp:
+            answer += 1
+            location -= 1
+            if deq:
+                first = max(deq)
+            if location < 0:
+                break
+            
+    return answer
